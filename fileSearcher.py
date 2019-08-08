b4d7b04217f1122507baf846bf3849d668e2824e
Michael Johnson <michael.johnson@soton.ac.uk>
2019-08-08 13:26:31 +0100

    modified:   bigTable.ipynb 	new file:   fileSearcher.py 	new file:   keplerLightCurves.ipynb 	modified:   projectGraphs.ipynb

---
+++ fileSearcher.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+
+textLocater = 'findName'
+
+dirlist = os.listdir('.')
+
+for i,item in enumerate(dirlist):
+	if item[0] != 0 and os.path.isfile(item):
+		f = open(item,'r')
+		searchString = f.read()
+		f.close()
+		indicater = searchString.find(textLocater)
+		if indicater != -1:
+			print(item+ ' - contains '+textLocater)
